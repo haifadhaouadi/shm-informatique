420f87a4a432ada7c42cbd5b6bcc7d62e8ff1104
haifa <dhawadihaifa@gmail.com>
2024-02-16 20:18:32 +0100

    first commit

---
+++ src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Haifa Dhaouadi',
+  email: 'dhawadihaifa@gmail.com',
+  photoURL: '/assets/images/avatars/haifa.jpg',
+};
